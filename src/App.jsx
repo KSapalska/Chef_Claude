4b329d4e8bf63245e5e0414d0584b71ae276f4a8
SC-80414 <SC-80414@MA-L4B5R48JRJCN>
2024-12-16 12:23:43 +0100

    form Object.fromEntries(formData)

--- src/App.jsx
+++ src/App.jsx
@@ -150,38 +150,102 @@ export default function App() {
 
  */
 
-/* export default function App() {
+ export default function App() {
   
-  function signUp(formData) {
-    
+  function signUp(event) {
+    event.preventDefault()
+    const formEl = event.currentTarget
+    const formData = new FormData(formEl)
    
-    const email= formData.get("email")
+  /*   const email= formData.get("email")
     const password = formData.get("password")
+    const employmentStatus = formData.get("employmentStatus")
+    const dietaryRestrictions = formData.getAll("dietaryRestrictions")
+    const favColor=formData.get("favColor")
 
-    console.log(email, password)
-   
+    formEl.reset()
+    console.log(favColor) */
+    
+    
+    const data = Object.fromEntries(formData)
+    const dietaryRestrictions = formData.getAll("dietaryRestrictions")
+    const allData = {
+      ...data,
+      dietaryRestrictions: dietaryRestrictions
+    }
+    
+    console.log(allData)
+    
+
   }
   
   return (
     <section>
       <h1>Signup form</h1>
-      <form action={signUp}>
-        <label htmlFor="email">Email:</label>
-        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
-        <br />
-        
+      <form onSubmit={signUp} method="post">
+      <label htmlFor="email">Email:</label>
+        <input id="email" defaultValue="joe@schmoe.com" type="email" name="email" placeholder="joe@schmoe.com" />
+
         <label htmlFor="password">Password:</label>
-        <input id="password" type="password" name="password" />
-        <br />
+        <input id="password" defaultValue="password123" type="password" name="password" />
+
+        <label htmlFor="description">Description:</label>
+        <textarea id="description" name="description" defaultValue="This is a description"></textarea>
+
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="Unemployed" />
+            Unemployed
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="full-time" defaultChecked={true}/>
+            Full-time
+        </label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Dietary restrictions:</legend>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" value="kosher" />
+            Kosher
+        </label>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" value="vegan" />
+            Vegan
+        </label>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="gluten-free" />
+            Gluten-free
+        </label>
+        </fieldset>
         
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <select id="favColor" name="favColor" defaultValue="" required>
+          <option value="" disabled>-- Choose a color --</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="indigo">Indigo</option>
+          <option value="violet">Violet</option>
+        </select>
+
         <button>Submit</button>
         
+
+
       </form>
     </section>
   )
-} */
+} 
 
-  import React from "react"
+/* import React from "react"
 
 export default function Main() {
 
@@ -217,4 +281,4 @@ export default function Main() {
             </ul>
         </main>
     )
-}
+} */
